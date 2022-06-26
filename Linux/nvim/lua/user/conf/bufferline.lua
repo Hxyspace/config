c663bb4f0c7c6da16a5b7c0c72881a2072e87078
yuan <yuanspace@qq.com>
2022-06-26 17:27:28 +0800

    1. set buffline backgroud color.
    2. fix:
        if accidentally quit without saving, errors will be repeated
            if press any key in neovim.

--- Linux/nvim/lua/user/conf/bufferline.lua
+++ Linux/nvim/lua/user/conf/bufferline.lua
@@ -11,10 +11,52 @@ require("bufferline").setup {
         }}
     },
     highlights = {
-        buffer_selected = {
+        fill = {
             guibg = '#9b9b9b'
         },
-        close_button_selected = {
+        background = {
+            guibg = '#9b9b9b'
+        },
+        buffer = {
+            guibg = '#9b9b9b'
+        },
+        tab = {
+            guibg = '#9b9b9b'
+        },
+        close_button = {
+            guibg = '#9b9b9b'
+        },
+        diagnostic = {
+            guibg = '#9b9b9b'
+        },
+        info = {
+            guibg = '#9b9b9b'
+        },
+        info_diagnostic = {
+            guibg = '#9b9b9b'
+        },
+        warning = {
+            guibg = '#9b9b9b'
+        },
+        warning_diagnostic = {
+            guibg = '#9b9b9b'
+        },
+        error = {
+            guibg = '#9b9b9b'
+        },
+        error_diagnostic = {
+            guibg = '#9b9b9b'
+        },
+        modified = {
+            guibg = '#9b9b9b'
+        },
+        duplicate = {
+            guibg = '#9b9b9b'
+        },
+        separator = {
+            guibg = '#9b9b9b'
+        },
+        pick = {
             guibg = '#9b9b9b'
         }
     }
--- Linux/nvim/lua/user/conf/nvim-tree.lua
+++ Linux/nvim/lua/user/conf/nvim-tree.lua
@@ -17,10 +17,11 @@ function get_load_list_bufs()
 end
 
 vim.api.nvim_create_autocmd("BufEnter", {
-  nested = true,
-  callback = function()
-    if #vim.api.nvim_list_wins() == 1 and vim.api.nvim_buf_get_name(0):match("NvimTree_") ~= nil then
-        vim.cmd "quit"
-    end
+    nested = true,
+    callback = function()
+        if #vim.api.nvim_list_wins() == 1 and vim.api.nvim_buf_get_name(0):match("NvimTree_") ~= nil then
+            vim.cmd "NvimTreeClose"
+            vim.cmd "quit"
+        end
   end
 })
