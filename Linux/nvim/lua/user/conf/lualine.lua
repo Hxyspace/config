955d8586e0b5e49aa113607aaa0235a21040c52f
yuan <yuanspace@qq.com>
2022-07-19 17:03:55 +0800

    add ascii art painting on startup

--- Linux/nvim/lua/user/conf/lualine.lua
+++ Linux/nvim/lua/user/conf/lualine.lua
@@ -2,7 +2,7 @@ local airlineish = require('user.conf.color').lualine_theme_airlineish()
 
 require('lualine').setup({
     options = { 
-        disabled_filetypes = {'NvimTree'},
+        disabled_filetypes = {'NvimTree', 'alpha'},
         theme = airlineish
     }
 })
